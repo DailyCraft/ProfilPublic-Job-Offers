2b6104712046b2a67cab9369414ca525d193f5d0
DailyCraft <51261780+DailyCraft@users.noreply.github.com>
2025-05-10 15:43:21 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul>
+    <li v-for="job in list">{{job.title}}</li>
+  </ul>
+</template>
+
+<script setup>
+const list = ref([]);
+
+const { data } = await useFetch("https://app.staging.profilpublic.fr/api/jobs");
+list.value.push(...data.value.data);
+</script>
