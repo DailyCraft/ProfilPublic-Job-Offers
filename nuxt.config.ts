d2d7ceca84335bca6b7f8d09da07f4b114d3e455
DailyCraft <51261780+DailyCraft@users.noreply.github.com>
2025-05-10 20:30:38 +0200

    Add GitHub workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
   css: [ "/public/main.css" ],
   app: {
     head: {
+      title: "Profil Public | Jobs",
       link: [
         { rel: "icon", type: "image/jpeg", href: "favicon.jpg" }
       ]
